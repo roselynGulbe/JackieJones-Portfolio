b63fb32877984189a45e8cd02b1fd460b2139cd7
JackJonesAM <jamatuguina.ccs@sccpag.edu.ph>
2024-01-29 22:26:47 +0800

    Yey

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,14 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Jack Jones A.M. </span>
+            from <span className="purple"> Zambonga del Sure, Philippines.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a student from Sain Columban College.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
-            <br />
+
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
@@ -24,17 +22,17 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Playing sports like volleyball and baseball
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Watching anime and kdrama
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Ka skeri baaaaaa!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Pretty, Jackie</footer>
         </blockquote>
       </Card.Body>
     </Card>
